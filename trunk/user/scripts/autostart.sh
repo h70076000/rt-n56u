a25f316d6c63bf8b84cd26d51bcc0e5c2d913a17
h70076000 <h70076222@hotmail.com>
2026-03-18 01:45:35 +0800

    Create autostart.sh

---
+++ trunk/user/scripts/autostart.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+#nvram set ntp_ready=0
+
+logger -t "autostart" "checking if the router is connected to the internet..."
+count=0
+while :
+do
+	ping -c 1 -W 1 -q 1.1.1.1 1>/dev/null 2>&1
+	if [ "$?" == "0" ]; then
+		break
+	fi
+	sleep 5
+	ping -c 1 -W 1 -q one.one.one.one 1>/dev/null 2>&1
+	if [ "$?" == "0" ]; then
+		break
+	fi
+	sleep 5
+	count=$((count+1))
+	if [ $count -gt 18 ]; then
+		break
+	fi
+done
+
+if [ $(nvram get gegoac_enable) = 1 ] ; then
+logger -t "自动启动" "正在启动巴法云物联网"
+/usr/bin/gegoac.sh start &
+fi
+
+if [ $(nvram get zerotier_enable) = 1 ] ; then
+logger -t "自动启动" "正在启动异地组网"
+/usr/bin/zerotier.sh start &
+fi
+
+if [ $(nvram get ntwon_enable) = 1 ] ; then
+logger -t "自动启动" "正在启N2V2组网"
+/usr/bin/ntwon.sh start &
+fi
+
+if [ $(nvram get etink_enable) = 1 ] ; then
+logger -t "自动启动" "正在启动ET异地组网"
+/usr/bin/etink.sh start &
+fi
+
+if [ $(nvram get etweb_enable) = 1 ] ; then
+logger -t "自动启动" "正在启动ETWEB异地组网"
+/usr/bin/etink.sh start &
+fi
