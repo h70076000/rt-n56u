06e3713d464c968079bc0038f78e5ec785429bc4
h70076000 <h70076222@hotmail.com>
2026-03-18 00:51:32 +0800

    Update common.h

--- trunk/user/httpd/common.h
+++ trunk/user/httpd/common.h
@@ -92,6 +92,8 @@ typedef u_int8_t u8;
 #define EVM_RESTART_SHADOWSOCKS	(1ULL << 48)
 #define EVM_RESTART_SS_TUNNEL		(1ULL << 49)
 #define EVM_RESTART_MENTOHUST		(1ULL << 50)
+#define EVM_RESTART_ZEROTIER		(1ULL << 51)
+#define EVM_RESTART_GECOAC		    (1ULL << 52)
 
 #define EVM_RESTART_REBOOT		(1ULL << 62)
 
@@ -151,6 +153,8 @@ typedef u_int8_t u8;
 #define EVT_RESTART_SS_TUNNEL		2
 #define EVT_RESTART_DNSFORWARDER	1
 #define EVT_RESTART_MENTOHUST		2
+#define EVT_RESTART_ZEROTIER    2
+#define EVT_RESTART_GECOAC	2
 #define EVT_RESTART_REBOOT		40
 
 struct variable
